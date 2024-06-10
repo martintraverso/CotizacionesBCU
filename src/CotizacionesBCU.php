bbc66306f2e5f4d2401d54e2b21bf55ffd6f93cf
martin_traverso <martin@traverso.com.uy>
2024-06-10 16:26:08 -0300

    Fix Curl without asking for certificated

--- src/CotizacionesBCU.php
+++ src/CotizacionesBCU.php
@@ -20,22 +20,30 @@ class CotizacionesBCU
 
 	private function getAPi(Periodo $periodo): mixed
 	{
+		$url = 'https://www.bcu.gub.uy/_layouts/15/BCU.Cotizaciones/handler/CotizacionesHandler.ashx?op=getcotizaciones';
+		$data = json_encode([
+			"KeyValuePairs" => [
+				"Monedas" => [["Val" => 0, "Text" => "TODAS"]],
+				"FechaDesde" => $periodo->getStart()->format('d/m/Y'), 
+				"FechaHasta" => $periodo->getEnd()->format('d/m/Y'), 
+				"Grupo" => "2"
+			]
+		]);
 		$curl = curl_init();
-		curl_setopt_array($curl, array(
-  			CURLOPT_URL => 'https://www.bcu.gub.uy/_layouts/15/BCU.Cotizaciones/handler/CotizacionesHandler.ashx?op=getcotizaciones',
-  			CURLOPT_RETURNTRANSFER => true,
-  			CURLOPT_ENCODING => '',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 0,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_POSTFIELDS =>'{"KeyValuePairs":{"Monedas":[{"Val":"0","Text":"TODAS"}],"FechaDesde":"' . $periodo->getStart()->format('d/m/Y') . '","FechaHasta":"' . $periodo->getEnd()->format('d/m/Y'). ',"Grupo":"2"}}',
-			CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
-));
-
-			$response = json_decode(curl_exec($curl), TRUE);
-			curl_close($curl);
-			return $response;	
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_ENCODING, '');	
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json', 'Content-Length: ' . strlen($data)]);
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, false );
+	
+		$response = json_decode(curl_exec($curl), TRUE);
+		var_dump(curl_errno($curl));
+		curl_close($curl);
+		return $response;	
 	}
 }
